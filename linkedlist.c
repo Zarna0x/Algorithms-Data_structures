d07daaac4c04b8f2124d188f23dba5590f024c7c
m1x3d art1st <justnightmare123@gmail.com>
2016-12-01 11:18:14 -0500

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -7,7 +7,8 @@ struct node{
 };
 
  //Head node
-
+// struct node* head;
+void InsertNth(int, int); // InserthNth(2,1)
 void Insert(struct node**,int);
 void Print(struct node*);
 
@@ -50,5 +51,28 @@ void Print (struct node* head) {
   }
 
   printf("\n\n");
+
+}
+
+void InsertNth (int data, int n) {
+  struct node* temp1  = (struct node*)malloc (sizeof(struct node)) ;
+  temp1->data = data;
+  temp1->next = NULL;
+  if (n == 1) {
+    temp1->next = head;
+    head = temp1;
+    return;
+  }
+
+  struct node* temp2 = (struct node*)malloc (sizeof(struct node)) ;
+  temp2 = head;
+  int i;
+  for ( i = 0; i < n-2; i++) {
+      temp2 = temp2->next;
+  }
+
+  temp1->next = temp2->next;
+  temp2->next = temp1;
+
 
 }

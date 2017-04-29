cf536a2807e652bdf6c6d021739549a4934fd850
Zarna0x <justnightmare123@gmail.com>
2017-04-29 19:22:14 +0400

    edit bubleSort

--- src/sorting_algorithms/bubbleSort.c
+++ src/sorting_algorithms/bubbleSort.c
@@ -5,7 +5,7 @@ void printArray(int *,int);
 int* bubbleSort(int *,int);
 int main () {
 
-int Arr[] = {5,7,9,1,3,15,30,2,3,5};
+int Arr[] = {5,7,9,1,3,15,30,2};
 int arraySize = sizeof(Arr)/sizeof(int);
 
 
@@ -36,19 +36,22 @@ void printArray(int *arr,int arrSize) {
 
 
 int* bubbleSort(int* Arr, int n) {
-  int i,j,temp;
+  int i,j,temp, swapped;
   for ( i = 0; i < n-1; i++) {
+     swapped = 0;
      for (j = 0; j< n-1-i; j++) {
+
         if (Arr[j] > Arr[j+1]) {
             //swap
             temp = Arr[j];
             Arr[j] = Arr[j+1];
             Arr[j+1] = temp;
-
+            swapped = 1;
         }
 
+
      }
-//     break;
+     if (swapped == 0) break;
   }
   return Arr;
 }
@@ -57,7 +60,7 @@ int* bubbleSort(int* Arr, int n) {
 /*
 Time Complexity  = O(n^2)
 
-T(n) = C(n-1) + C(n-2) + C(n-3) + C(n-(n-1)
+T(n) = C(n-1) + C(n-2) + C(n-3) + C(n-(n-1))
 = C(n(n-1)/2) = O(n^2)
 
 */
